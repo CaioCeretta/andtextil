133ec7c36ef589b4631bd4579216faf9cd027e95
Caio Ceretta <caioceretta@gmail.com>
2025-04-20 23:52:37 -0300

    .

--- next/src/app/api/products/route.ts
+++
@@ -1,7 +0,0 @@
-import { getProducts } from '@/data/dal/products/get-products'
-
-export async function GET() {
-  const products = await getProducts()
-
-  return new Response(JSON.stringify(products), { status: 200 })
-}
