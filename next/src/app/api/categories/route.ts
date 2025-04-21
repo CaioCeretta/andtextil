207b55ad0d115c65bdb45a025e03c6a9e9a08989
Caio Ceretta <caioceretta@gmail.com>
2025-04-20 23:40:21 -0300

    chore: test

--- next/src/app/api/categories/route.ts
+++ next/src/app/api/categories/route.ts
@@ -1,9 +1,10 @@
+import { getCategories } from '@/data/dal/categories/get-categories'
 import { db } from '@/lib/prisma'
 import { NextResponse } from 'next/server'
 
 export async function GET() {
   try {
-    const categories = await db.category.findMany()
+    const categories = await getCategories()
     return NextResponse.json(categories)
   } catch (error) {
     console.error('Erro ao buscar categorias:', error)
