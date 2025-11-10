9ee94f9dae585d1e048a4bccb057344b3a740b6a
CaioCeretta <caioceretta@gmail.com>
2025-11-10 13:36:18 -0300

    refactor: remove unusued useEffect, organized imports and add semicolon

--- src/contexts/ProductsContext.tsx
+++ src/contexts/ProductsContext.tsx
@@ -1,40 +1,29 @@
-'use client'
+"use client";
 
-import type { ProductsContextProps } from '@/shared/interfaces'
-import type { ProductType } from '@/shared/types'
-import { createContext, useEffect, useMemo, useState } from 'react'
-import useCategories from '../hooks/useCategories'
+import { createContext, useEffect, useMemo, useState } from "react";
+import type { ProductsContextProps } from "@/shared/interfaces";
+import type { ProductType } from "@/shared/types";
+import useCategories from "../hooks/useCategories";
 
-export const ProductsContext = createContext<ProductsContextProps>({} as any)
+export const ProductsContext = createContext<ProductsContextProps>({} as any);
 
 export const ProductsProvider = (props: any) => {
-  const [products, setProducts] = useState<ProductType[]>([])
-  const { selectedCategory } = useCategories()
-
-  useEffect(() => {
-    const fetchProducts = async () => {
-      const res = await fetch('/api/products')
-      const data = await res.json()
-
-      setProducts(data)
-    }
-
-    fetchProducts()
-  }, [])
+  const [products, setProducts] = useState<ProductType[]>([]);
+  const { selectedCategory } = useCategories();
 
   const filteredProducts = useMemo(
     () =>
       selectedCategory
         ? products.filter(
-            (product: ProductType) => product.categoryId === selectedCategory,
-          )
+          (product: ProductType) => product.categoryId === selectedCategory,
+        )
         : products,
     [selectedCategory, products],
-  )
+  );
 
   return (
     <ProductsContext.Provider value={{ products: filteredProducts }}>
       {props.children}
     </ProductsContext.Provider>
-  )
-}
+  );
+};
